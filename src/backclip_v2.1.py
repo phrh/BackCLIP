780852074022e780785e8f89d1fdca0bffb69385
Paula <phreyes@gmail.com>
2015-06-28 15:55:36 -0500

    remove option, proportion ci and rsize

--- src/backclip_v2.1.py
+++ src/backclip_v2.1.py
@@ -1,41 +1,18 @@
-# BackCLIP - identifies the presence of common background in a PAR-CLIP dataset.
-#
-# Created by Paula H. Reyes-Herrera, Cesar A. Speck Hernandez, Carlos Sierra, Santiago Herrera on 5 February 2015
-# Copyright (c) 2015 Paula H. Reyes-Herrera, Cesar A. Speck Hernandez, Carlos Sierra, Santiago Herrera. All rights reserved.
-#
-#
-# BackCLIP is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, version 2.
 #-------------------------------------------------------------------------------------
 #!/usr/bin/env python
 # USAGE: python backclip.py filebackground fileclusters fileparameters
-# Use this program to have a quantitative measure of the amount of background present in a determined dataset.
+# Use this program to have a quantitative measure of the amount of background present in a determined dataset. The presence of background is considerable 
 # INPUT:         fileparameters -> file with parameters such as:
-#		 	alpha=significance level(default value 0.01)
-#		 	threshold=to define if the amount of common background is significant (default (maximum score)/2) 
-#		 	histogram=give as output histogram of the intersection scores (default false)
-#			fbackground=(.BED)file background and corresponding scores
-#			fclusters=(.BED)file with clusters detected from PAR-CLIP dataset	
-#		 	filename=in case histogram is true, the name of the output file (default fileclusters.histbackground)	
-#			namebed=name bed with set of common background found in fileclusters
+#		 alpha: significance level(default value 0.01)
+#		 threshold: to define if the amount of common background is significant (default (maximum score)/2) 
+#		 histogram: give as output histogram of the intersection scores (default false)
+#		 remove: remove sites in the intersection with score > threhsold from dataset. 
+#		 	The output file name is clusters file name .backclip.bed (default false)
+#		 filename: in case histogram is true, the name of the output file (default fileclusters.histbackground)	
 # OUTPUT: confidence interval (significance level 99%)
-# 	  histogram score distribution in the intersection file (saved in filename)
-#	  common background in file clusters (saved as namebed)
+# 	  histogram score distribution in the intersection file
+#	  According to the user input parameters, the dataset without the sites in the intersection (score > threshold)		
 #-------------------------------FUNCTIONS-LIST----------------------------------------
-#	getparameters(parameters)
-#	getstatistics(scores,alpha,threshold)
-#	findseq(chrom,startp,endp,seq,strand,fclu)
-#	findscore(chrom,startp,endp,seq,strand,fint2)
-#	clean_intersection(fintfil,fclu,fint2)
-#	getthreshold(fback)
-#	createhistogram(scores_hist,figurename,nbin,file1)
-#	DeleteDuplicatesList(x)
-#	deleteandmerge(bedint)
-#	check_overlap(lineas,chrom,startp,endp,score,strand,nline)
-#	process_inputfile(parameters)
-#	int_main(parameters) 
-#---------------------------------FUNCTIONS-------------------------------------------
 
 def getparameters(parameters):
 	from datetime import date
@@ -45,9 +22,11 @@ def getparameters(parameters):
 	alpha=0.01
 	histogram=False
 	smotif=False
+	motif=''
 	filename='histbackground '+str(date.today().ctime())
 	namebed=filename+'intersection.bed'
-	threshold=0
+	remove=False
+	
 	for line in lineas:
 		paramname,value=line.split("=")
 		if paramname.lower().strip() == "alpha" :
@@ -57,37 +36,53 @@ def getparameters(parameters):
 				histogram=True
 		elif paramname.lower().strip() == "filename" :		
 			filename=value.strip()
+		elif paramname.lower().strip() == "smotif" :
+			smotif=True
+		elif paramname.lower().strip() == "motif" :
+			motif=value.strip()
 		elif paramname.lower().strip() == "namebed" :
 			namebed=value.strip()+'.intersection.bed'
 		elif paramname.lower().strip() == "fbackground" :
 			fbackground=value.strip()
 		elif paramname.lower().strip() == "fclusters" :
 			fclusters=value.strip()
-		elif paramname.lower().strip() == "threshold" :
-			threshold=int(value.strip())
-	return alpha,histogram,filename,namebed,fbackground,fclusters,threshold
+		elif paramname.lower().strip() == "remove" :
+			if value.lower().strip() == "true" :
+				remove=True
+	return alpha,histogram,filename,smotif,motif,namebed,fbackground,fclusters,remove
 
 
 def getstatistics(scores,alpha,threshold):
 	import scipy
 	from scipy import stats	
-#	confidence intervals
+	import math
 	if len(scores) >= 30:
-		ascores=scipy.array(scores)
-		sem=scipy.stats.sem(ascores)
-		samplemean=ascores.mean()
+#Indicator 1 - Confidence interval for the proportion
+		hscores=filterarray(scores,threshold)
+		lenhscores=len(hscores)
+		lenscores=len(scores)
+		ascores=scipy.array(scores)	
+		p=float(lenhscores)/float(lenscores)
+		x=math.sqrt(p*(1-p)/float(lenscores))		
 		if alpha==0.01 :
 			factor=2.58
-			ci=[samplemean-factor*sem,samplemean+factor*sem]
+			ci=[p-factor*x,p+factor*x]
+			ci=[x*100 for x in ci]
 		elif alpha==0.05 :
 			factor=1.96
-			ci=[samplemean-factor*sem,samplemean+factor*sem]
+			ci=[p-factor*x,p+factor*x]
+			ci=[x*100 for x in ci]
 		else :
 			print "sample size must be >30"
 			ci=[]
-		[t,pvalue]=scipy.stats.ttest_1samp(ascores,threshold)
-	return ci,pvalue/2
+	return ci
 
+def filterarray(array,filternumber):
+	import numpy
+	myarray=numpy.asarray(array)
+	arrayfilteredindexes=myarray>filternumber
+	arrayfiltered=myarray[arrayfilteredindexes].tolist()
+	return arrayfiltered
 
 def findseq(chrom,startp,endp,seq,strand,fclu):
 	flag=0
@@ -107,27 +102,42 @@ def findscore(chrom,startp,endp,seq,strand,fint2):
 			newscore=interval.score
 	return newscore
 
-
-def clean_intersection(fintfil,fclu,fint2):
+def clean_intersection(fintfil,fclu,fint2,smotif):
 	import pybedtools
 	items=[]
 	scores=[]
 	for interval in fintfil :
 		chrom,startp,endp,seq,score,strand=interval.chrom,interval.start,interval.end,interval.name,interval.score,interval.strand
-		newseq=findseq(chrom,startp,endp,seq,strand,fclu)
+		if smotif == True :
+			newseq=findseq(chrom,startp,endp,seq,strand,fclu)
+		else :
+			newseq='NA'
 		newscore=findscore(chrom,startp,endp,seq,strand,fint2)
 		items.append((chrom,startp,endp,newseq,newscore,strand))
 		scores.append(int(newscore))
 	newfintfil=pybedtools.BedTool(items)
 	return newfintfil,items,scores
 
+def filterarray(array,filternumber):
+	import numpy
+	myarray=numpy.asarray(array)
+	arrayfilteredindexes=myarray>filternumber
+	arrayfiltered=myarray[arrayfilteredindexes].tolist()
+	return arrayfiltered
 
+# score based on the 99 percentile
 def getthreshold(fback):
+	import scipy 
+	from scipy import stats	
 	scores=[]
+	ifilter=1
 	for interval in fback :
 		scores.append(int(interval.score))
-	threshold=(round(max(scores)/2))
-	return threshold
+	array2=filterarray(scores,ifilter)
+	spscoresback=scipy.array(array2)
+	percentile=stats.scoreatpercentile(array2,99)
+	return percentile	
+
 
 def createhistogram(scores_hist,figurename,nbin,file1):
 	import numpy
@@ -150,10 +160,15 @@ def DeleteDuplicatesList(x):
     return cleanbed,inputlist
 
 
+def countmotifs(fclu):
+	iupacdict = {'M':'[AC]','R':'[AG]','W':'[AT]','S':'[CG]','Y':'[CT]','K':'[GT]','V':'[ACG]','H':'[ACT]','D':'[AGT]','B':'[CGT]','X':'[ACGT]','N':'[ACGT]'}    
+        for key, value in iupacdict.iteritems():
+                motif=motif.replace(key,value)	
+
 
 def deleteandmerge(bedint):
 	import pybedtools
-# Remove short sequences (l <10)
+# Remove short sequences (l <10()
 	nline=0;
 	total_lineas=len(bedint)
 	outbed=[]
@@ -215,18 +230,17 @@ def check_overlap(lineas,chrom,startp,endp,score,strand,nline):
 			flag=1
 	return startp,endp,score,nreads
 
-	
-
+#def process_inputfile(fbackground,fclusters,parameters):
 def process_inputfile(parameters):
-	[alpha,histogram,filename,namebed,fbackground,fclusters,threshold]=getparameters(parameters)
+	[alpha,histogram,filename,smotif,motif,namebed,fbackground,fclusters,remove]=getparameters(parameters)
 	import pybedtools
 	print "filebackground --- "+fbackground
 	print "fileclusters --- "+fclusters
 	fback=pybedtools.BedTool(fbackground)
 	fclu =pybedtools.BedTool(fclusters)
-# get score threshold (default)
-	if threshold==0 :
-		threshold=getthreshold(fback)	
+	totalclusters=len(fclu)
+# get score threshold (default) - 99 percentile
+	threshold=getthreshold(fback)	
 # first file must have the sequence
 	fint1=fclu.intersect(fback)
 	fint2=fback.intersect(fclu)
@@ -239,10 +253,15 @@ def process_inputfile(parameters):
 # check overlap 
 	fintfil=deleteandmerge(fint1list)
 # obtain smaller sequences
-	newfintfil,items,scores=clean_intersection(fintfil,fclu,fint2)
+	newfintfil,items,scores=clean_intersection(fintfil,fclu,fint2,smotif)
 	newfintfil.saveas(namebed)
-# statistics from bed
-	ci,pvalue=getstatistics(scores,alpha,threshold)
+# Remvove sites above threshold from dataset 
+	if remove == True : 
+		newdataset=fclu.subtract(newfintfil)
+		newdataset.saveas(fclusters+'.backclip.bed')
+# Indicator 2
+	rsize=float(float(len(scores))/float(totalclusters))*100
+	ci=getstatistics(scores,alpha,threshold)
 # Histogram from bed	
 	if histogram == True :
 # n bin set to the maximum score
@@ -250,19 +269,22 @@ def process_inputfile(parameters):
 # hist title is the filename, but this can be modified
 		histtitle=filename
 		createhistogram(scores,filename,nbin,histtitle)
-	return ci,threshold,alpha
+	return ci,threshold,alpha,rsize
 
 
 def int_main(parameters):
 	print "fileparameters --- "+parameters
-	ci,threshold,alpha=process_inputfile(parameters)
-	print "Result:"
-	if ci[0]> threshold or ci[1]>threshold :
-		print "Unusual presence of common background, ci > Threshold, (Threshold ="+str(threshold)+")"
-		print "Confidence interval for the mean of the score (alpha="+str(alpha)+"), ci:"+str(ci) 
+	ci,threshold,alpha,rsize=process_inputfile(parameters)
+	print "Result:"	
+	if ci[0]> 50 or ci[1]>50 :
+		print "Unusual presence of common background, ci proportion> Threshold, (Threshold =50%)"
+		print "Confidence interval for the proportion of sites with a score higher than the threshold(alpha="+str(alpha)+"), ci:"+str(ci) 
 	else :
-		print "Usual presence of common background, ci < Threshold, (Threshold ="+str(threshold)+")"
-		print "Confidence interval for the mean of the score (alpha="+str(alpha)+"), ci:"+str(ci) 
+		print "Confidence interval for the proportion of sites with a score higher than the threshold(alpha="+str(alpha)+"), ci:"+str(ci)
+	if rsize>30 :
+		print "Unusual number of sites in the intersection (with the common background) relative to the number of clusters"+str(rsize)
+	else :
+		print "number of sites in the intersection (with the common background) relative to the number of clusters"+str(rsize)
 
 
 #----------------------------------MAIN-------------------------------------------
